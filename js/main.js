9bc97fb1114bdb2c5b1a788789af849ed44928be
nate <student@epicodus.com>
2015-10-01 16:48:45 -0700

    on tut 3

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var game = new Phaser.Game(640, 360, Phaser.AUTO);
+var game = new Phaser.Game(1782, 600, Phaser.AUTO);
 
 var GameState = {
   preload: function() {
-
+    this.load.image('background', 'assets/images/background.png')
   },
   create: function () {
-
+    this.background = this.game.add.sprite(0, 0, 'background');
   },
   update: function () {
 
